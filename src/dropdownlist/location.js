c9d984809254008a7d61d02354eff3dc31dab378
KrittimaL <72492293+KrittimaL@users.noreply.github.com>
2020-10-21 12:19:42 +0700

    Add files via upload

---
+++ src/dropdownlist/location.js
@@ -0,0 +1,14 @@
+
+const locationlist = [
+  {id: 1, name: 'HM Building'},
+  {id: 2, name: 'E12 Building'},
+  {id: 3, name: 'Industrial Department'},
+];
+
+export default locationlist;
+
+export const locationitems = [
+  {label: 'HM Building',value:'HM Building' }, 
+  {label: 'E12 Building',value: 'E12 Building'}, 
+  {label: 'Industrial Department',value: 'Industrial Department'},
+]
